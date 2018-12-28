34060009a011fa004217f0dd341be6b9dafaacc6
Karl Johnson <karl.michael.johnson@gmail.com>
2018-12-28 15:29:42 -0500

    Render results in a new view, 'dogs', with a GET request

--- find_a_doggo/views.py
+++ find_a_doggo/views.py
@@ -1,6 +1,6 @@
 """Store the views."""
 
-from flask import request, render_template
+from flask import redirect, request, render_template, url_for
 
 from find_a_doggo.breed_urls import BREED_URLS
 from find_a_doggo import app
@@ -10,20 +10,29 @@ from find_a_doggo.helpers import searcher
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    """Return results if valid POST request; render landing page otherwise."""
+    """Redirect to 'found' if valid form; render landing page otherwise."""
     form = SearchForm()
 
     if request.method == 'POST' and form.validate():
         breed_type = request.form['breed_type']
         location = request.form['location']
-        results = searcher(breed_type=breed_type, location=location)
-        result_dict = {'results': results,
-                       'location': location,
-                       'breed_type': breed_type,
-                       'breed_urls': BREED_URLS}
-        return render_template("results.html", **result_dict)
+        return redirect(url_for('dogs',
+                                location=location,
+                                breed_type=breed_type))
     else:
         return render_template('landing.html', form=form)
+
+
+@app.route('/dogs', methods=['GET'])
+def dogs():
+    """Return the found results."""
+    breed_type = request.args.get('breed_type')
+    location = request.args.get('location')
+    results = searcher(breed_type=breed_type,
+                       location=location)
+    return render_template('results.html',
+                           results=results,
+                           breed_urls=BREED_URLS)
 
 
 @app.route('/about', methods=['GET'])
